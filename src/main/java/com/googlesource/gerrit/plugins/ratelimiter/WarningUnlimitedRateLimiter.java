7752ae82a0871abcae6ad43be90d5224e8203e34
Mark Bekhet <mark.bekhet@ericsson.com>
2021-03-29 15:01:41 -0400

    Introduce a configuration to define time lapse for rate limiter
    Before this change, rate limiter was executed every hour. Sometimes, a
    sudden high memory demand on a short period of time happens caused by
    high number of clones and fetches. Also, the user gets blocked from
    fetching information due to reaching the limit per hour.
    
    This change introduces an optional configurable field "timelapse". This
    field defines a duration in minutes supported by the rate-limiter
    plugin. This helps address the potential problem of sudden high memory
    demand by configuring the number of uploaded packs over smaller amount
    of time. If this field is not defined or is defined for a value higher
    than or equal to 60, the default value of 60 minutes is established and
    a warning message is logged.
    
    This change also adapts the warnings provided by the rate limiter to
    reflect the time lapse of the plugin.
    
    Bug: Issue 10302
    Change-Id: I5cdbb931d676c7630178d0b8ba0245e3c8b774a8

--- src/main/java/com/googlesource/gerrit/plugins/ratelimiter/WarningUnlimitedRateLimiter.java
+++ src/main/java/com/googlesource/gerrit/plugins/ratelimiter/WarningUnlimitedRateLimiter.java
@@ -20,10 +20,11 @@ import com.google.inject.assistedinject.Assisted;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 
-class WarningHourlyUnlimitedRateLimiter implements RateLimiter {
+class WarningUnlimitedRateLimiter implements RateLimiter {
   @FunctionalInterface
   interface Factory {
-    WarningHourlyUnlimitedRateLimiter create(RateLimiter delegate, String key, int warnLimit);
+    WarningUnlimitedRateLimiter create(
+        RateLimiter delegate, String key, int warnLimit, long timeLapse);
   }
 
   private static final Logger rateLimitLog = RateLimiterStatsLog.getLogger();
@@ -32,18 +33,21 @@ class WarningHourlyUnlimitedRateLimiter implements RateLimiter {
   private final RateLimiter delegate;
   private final int warnLimit;
   private final String key;
+  private final long timeLapse;
   private volatile boolean warningWasLogged = false;
 
   @Inject
-  WarningHourlyUnlimitedRateLimiter(
+  WarningUnlimitedRateLimiter(
       UserResolver userResolver,
       @Assisted RateLimiter delegate,
       @Assisted String key,
-      @Assisted int warnLimit) {
+      @Assisted int warnLimit,
+      @Assisted long timeLapse) {
     this.userResolver = userResolver;
     this.delegate = delegate;
     this.warnLimit = warnLimit;
     this.key = key;
+    this.timeLapse = timeLapse;
   }
 
   @Override
@@ -57,9 +61,10 @@ class WarningHourlyUnlimitedRateLimiter implements RateLimiter {
 
     if (acquirePermit && (usedPermits() == warnLimit)) {
       rateLimitLog.info(
-          "{} reached the warning limit of {} uploadpacks per hour.",
+          "{} reached the warning limit of {} uploadpacks per {} minutes.",
           userResolver.getUserName(key).orElse(key),
-          warnLimit);
+          warnLimit,
+          timeLapse);
       warningWasLogged = true;
     }
     return acquirePermit;
