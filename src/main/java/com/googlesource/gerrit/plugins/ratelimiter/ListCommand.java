b78854784237d7f9d8658475b35e949ab8c47f2e
Nguyen Tuan Khang Phan <phan.khang5@gmail.com>
2022-02-23 12:40:16 -0500

    Add REST Interface to list and replenish limits
    Implement a REST API that is the equivalent of the ssh "list" and
    "replenish" commands detailed in the existing rate-limiter plugin
    documentation. To mimic the functionality of the ssh command, keywords
    are used in the endpoints and options are provided as parameters.
    
    Feature: Issue 10304
    Change-Id: I16753b26b994109b25134605f577fc688bb46060

--- src/main/java/com/googlesource/gerrit/plugins/ratelimiter/ListCommand.java
+++ src/main/java/com/googlesource/gerrit/plugins/ratelimiter/ListCommand.java
@@ -16,19 +16,12 @@ package com.googlesource.gerrit.plugins.ratelimiter;
 import static com.google.gerrit.sshd.CommandMetaData.Mode.MASTER_OR_SLAVE;
 import static com.googlesource.gerrit.plugins.ratelimiter.Module.UPLOAD_PACK_PER_HOUR;
 
-import com.google.common.cache.LoadingCache;
 import com.google.gerrit.common.data.GlobalCapability;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
 import com.google.gerrit.sshd.AdminHighPriorityCommand;
 import com.google.gerrit.sshd.CommandMetaData;
 import com.google.gerrit.sshd.SshCommand;
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
-import java.time.Duration;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 @AdminHighPriorityCommand
 @RequiresCapability(GlobalCapability.ADMINISTRATE_SERVER)
@@ -37,18 +30,15 @@ import java.util.concurrent.TimeUnit;
     description = "Display rate limits statistics",
     runsAt = MASTER_OR_SLAVE)
 final class ListCommand extends SshCommand {
-  private static final String FORMAT = "%-26s %-17s %-19s %-15s %s";
   private static final String DASHED_LINE =
       "---------------------------------------------------------------------------------------------";
-  private final LoadingCache<String, RateLimiter> uploadPackPerHour;
-  private final UserResolver userResolver;
+  private final RateLimiterProcessing rateLimiterProcessing;
+
+  static final String FORMAT = "%-26s %-17s %-19s %-15s %s";
 
   @Inject
-  ListCommand(
-      @Named(UPLOAD_PACK_PER_HOUR) LoadingCache<String, RateLimiter> uploadPackPerHour,
-      UserResolver userResolver) {
-    this.uploadPackPerHour = uploadPackPerHour;
-    this.userResolver = userResolver;
+  ListCommand(RateLimiterProcessing rateLimiterProcessing) {
+    this.rateLimiterProcessing = rateLimiterProcessing;
   }
 
   @Override
@@ -66,32 +56,10 @@ final class ListCommand extends SshCommand {
               "Used Permits",
               "Replenish in"));
       stdout.println(DASHED_LINE);
-      uploadPackPerHour.asMap().entrySet().stream()
-          .sorted(Map.Entry.comparingByValue())
-          .forEach(this::printEntry);
+      stdout.println(rateLimiterProcessing.listPermits());
       stdout.println(DASHED_LINE);
     } catch (Exception e) {
       throw die(e);
     }
-  }
-
-  private void printEntry(Entry<String, RateLimiter> entry) {
-    stdout.println(
-        String.format(
-            FORMAT,
-            getDisplayValue(entry.getKey()),
-            permits(entry.getValue().permitsPerHour()),
-            permits(entry.getValue().availablePermits()),
-            permits(entry.getValue().usedPermits()),
-            Duration.ofSeconds(entry.getValue().remainingTime(TimeUnit.SECONDS))));
-  }
-
-  private String permits(int value) {
-    return value == Integer.MAX_VALUE ? "unlimited" : Integer.toString(value);
-  }
-
-  private String getDisplayValue(String key) {
-    Optional<String> currentUser = userResolver.getUserName(key);
-    return currentUser.map(name -> key + " (" + name + ")").orElse(key);
   }
 }
