5195c0f99f3f758ab723def919f7be531830cd92
Phan Tran Tuan Khai <tran.tuan.khai.phan@ericsson.com>
2022-12-12 18:35:00 -0500

    Adding option to specify config in All-Projects
    Now it is possible to change configuration of rate limiter in
    All-Projects configuration.
    
    To change configuration of rate limiter one needs to add
    rate-limiter.config file to All-Projects refs/meta/config. If there is
    no rate-limiter.config in All-Projects then the configuration will be
    taken from etc/rate-limiter.config.
    
    Change-Id: Ie412837fb23d9c5c7e27e9edf5c0f92cf864ae57

--- src/main/java/com/googlesource/gerrit/plugins/ratelimiter/PeriodicRateLimiter.java
+++ src/main/java/com/googlesource/gerrit/plugins/ratelimiter/PeriodicRateLimiter.java
@@ -14,6 +14,7 @@
 
 package com.googlesource.gerrit.plugins.ratelimiter;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import java.util.Optional;
@@ -29,9 +30,9 @@ class PeriodicRateLimiter implements RateLimiter {
   private final Semaphore semaphore;
   private final int maxPermits;
   private final AtomicInteger usedPermits;
-  private final ScheduledFuture<?> replenishTask;
   private final String rateLimitType;
   private final int timeLapse;
+  private ScheduledFuture<?> replenishTask;
 
   interface Factory {
     PeriodicRateLimiter create(int permits, long timeLapse, String rateLimitType);
@@ -51,6 +52,11 @@ class PeriodicRateLimiter implements RateLimiter {
     this.replenishTask =
         executor.scheduleAtFixedRate(
             this::replenishPermits, timeLapse, timeLapse, TimeUnit.MINUTES);
+  }
+
+  @VisibleForTesting
+  public void setReplenishTask(ScheduledFuture<?> replenishTask) {
+    this.replenishTask = replenishTask;
   }
 
   @Override
